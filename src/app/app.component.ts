a51c52ece086ee271c06798c63a8080ce43c6959
Esteban Hernandez <hernandez.dev.services@gmail.com>
2020-06-08 17:40:17 -0400

    Added comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, Form } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,23 +14,48 @@ export class AppComponent implements OnInit {
     private readonly http: HttpClient,
     private readonly snackBar: MatSnackBar
   ) {}
-
+  /**
+   * The application's title.
+   */
   title = 'retry-on-error-demo';
-
+  /**
+   * True when the application is loading.
+   */
   isLoading: boolean;
-
+  /**
+   * True when the user is authenticated.
+   */
   isLoggedIn: boolean;
-
+  /**
+   * True when the authentication service
+   * has returned an error.
+   */
   hasError: boolean;
-
+  /**
+   * The error message returned by the
+   * authentication service.
+   */
   errorMessage: string;
-
-  email: FormControl;
-
-  password: FormControl;
-
+  /**
+   * The login form group.
+   */
   formGroup: FormGroup;
-
+  /**
+   * The email form control.
+   */
+  get email(): FormControl {
+    return this.formGroup.get('email') as FormControl;
+  }
+  /**
+   * The password form control.
+   */
+  get password(): FormControl {
+    return this.formGroup.get('password') as FormControl;
+  }
+  /**
+   * Initialize the template state
+   * and build the login form.
+   */
   ngOnInit(): void {
     this.isLoading = false;
     this.isLoggedIn = false;
@@ -42,41 +68,61 @@ export class AppComponent implements OnInit {
         Validators.minLength(8),
       ]),
     });
-    this.email = this.formGroup.get('email') as FormControl;
-    this.password = this.formGroup.get('password') as FormControl;
   }
-
+  /**
+   * Authenticate the user through
+   * the authentication service using the
+   * current state of the login form as
+   * the user's credentials.
+   */
   login(): void {
     this.isLoading = true;
-    this.authenticate();
-  }
-
-  logout(): void {
-    this.formGroup.reset();
-    this.isLoggedIn = false;
-  }
-
-  private authenticate(): void {
     this.http
       .post<any>('http://localhost:3000/login', this.formGroup.value)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          this.formGroup.reset();
+        })
+      )
       .subscribe({
         next: this.onAuthenticationSuccess.bind(this),
         error: this.onAuthenticationError.bind(this),
       });
   }
-
+  /**
+   * Revoke the user's existing
+   * authentication.
+   */
+  logout(): void {
+    this.isLoggedIn = false;
+  }
+  /**
+   * Called asynchronously when the user's authentication
+   * attempt succeeds.
+   *
+   * It will set the user's authenticated state
+   * to true and remove any pending error
+   * messages.
+   */
   private onAuthenticationSuccess(): void {
-    this.isLoading = false;
     this.isLoggedIn = true;
     this.hasError = false;
     this.errorMessage = null;
   }
-
+  /**
+   * Called asynchronously when the users' authentication
+   * attempt fails.
+   *
+   * It will show a notification with the status text of
+   * the response and set an error message to be displayed
+   * under the form.
+   *
+   * @param error An HTTP Response error with failure details.
+   */
   private onAuthenticationError(error: HttpErrorResponse): void {
     this.snackBar.open(error.statusText, 'Dismiss');
-    this.isLoading = false;
     this.hasError = true;
     this.errorMessage = 'Account was not found';
-    this.formGroup.reset();
   }
 }
